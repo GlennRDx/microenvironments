8b6ade12ed3dba28f05edbc7744f52f38fdcf235
GlennRDx <glennrossdolan@gmail.com>
2025-11-29 16:32:39 +0100

    Added config file for easy/safe parameter adjustments

---
+++ config.R
@@ -0,0 +1,59 @@
+# Paths and directories
+data_dir <- "/home/glennrdx/Documents/lab_projects/microenvironments/01_data"
+figs_dir   <- "/home/glennrdx/Documents/lab_projects/microenvironments/03_figures"
+
+# General parameters
+classification_method <- "surface_class_radius" # Options: "surface_class_nn", "surface_class_clusters", "surface_class_radius"
+
+# 0.1.1
+wts_params <- list(
+  n_subsamples = 1000, # Number of cells to keep per cell type (to reduce computational load)
+  n_top_genes = 4000,   # Number of highly deviant genes for selection
+  n_dims    = 1:15     # Number of dimensions to use for dimensionality reduction techniques (PCA, UMAP, t-SNE). Number is based on elbow plot.
+)
+
+# 0.2.1
+iss_params <- list(
+  radius      = 35,    # Radius for neighbourhood definition
+  knn         = 25,    # Number of neighbours for KNN method
+  center_x    = 3250,  # Center X coordinate for zooming/plotting
+  center_y    = 2075,  # Center Y coordinate for zooming/plotting
+  threshold   = 0.85,  # Threshold for Core vs Surface classification
+  zoom_radius = 50     # Default radius for zoom plots
+)
+
+# 0.3.1
+lda_params <- list(
+  seed        = 123,
+  train_prop  = 2/3,   # Proportion of shared genes for training
+  split_prop  = 0.8    # Train/Test split ratio for cells
+)
+
+# 0.3.2
+svr_params <- list(
+  seed        = 123,
+  train_split = 0.7,
+  kernel      = "linear",
+  type        = "eps-regression"
+)
+
+# 0.3.3
+rf_params <- list(
+  seed        = 42,
+  n_x_bins    = 5,    # Spatial bins for cross-validation
+  n_y_bins    = 5,
+  train_split = 0.8,
+  ntree       = 500,
+  nodesize    = 5
+)
+
+# 0.4.1
+prediction_params <- list(
+  prob_threshold = 0.85 # Probability threshold for surface/core predictions
+)
+
+# 0.5.1
+validation_params <- list(
+  n_pseudo_reps = 2,      # Number of pseudo-replicates
+  marker_method = "MAST"  # Options: "MAST", "wilcox", "DESeq2"
+)
